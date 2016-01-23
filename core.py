061ba37acdf937e5b8a135460bc3db0f6b058a97
secknv <secknv@gmail.com>
2016-01-23 17:54:23 +0000

    added help cmd

--- core.py
+++ core.py
@@ -12,20 +12,20 @@ if input('Configure?(y/n)') == 'y':
 print('Connecting to discord servers...')
 '''
 
-# specific test channel id
 allowed_channels = ['140225706230677504']
 
 
 @client.event
 async def on_message(message):
 
-    cmd_array = dir(pymods)
+    cmd_array = [s for s in dir(pymods) if 'py_' in s]
     cmd = '_'.join(message.content.split())
 
     if message.channel.id in allowed_channels:
-        # just a debug function
-        if cmd == 'pymoddir':
-            await client.send_message(message.channel, str(dir(pymods)) + '\n' + str(cmd_array))
+
+        # list commands
+        if cmd == 'py_help':
+            await client.send_message(message.channel, '```Available commands are:\n' + '\n'.join(cmd_array) + '```')
 
         # chats the return of any function in the module file
         if cmd in cmd_array:
