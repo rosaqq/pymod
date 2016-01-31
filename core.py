0d45bf8e018f32870570539d5fe95fa42868e955
secknv <secknv@gmail.com>
2016-01-31 19:53:38 +0000

    lotsa new stuff

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ import sys
 import os
 import configparser
 from types import FunctionType
+import inspect
 
 config = configparser.ConfigParser()
 config.read("pymod.ini")
@@ -18,7 +19,7 @@ if input('Configure?(y/n)') == 'y':
 print('Connecting to discord servers...')
 '''
 
-allowed_channels = ['140225706230677504']
+allowed_channels = ['140225706230677504', '143055883755192321', '128470036980563968']
 
 # module loader
 # ----------------------------------------------------------------------------------------------------------------------
@@ -40,23 +41,31 @@ for i in valid_list:
     exec(i + " = c." + i)
 
 cmd_dict = {}
+print(valid_list)
 for v in valid_list:
     cmd_list = [x for x, y in eval(v + '.__dict__.items()') if type(y) == FunctionType and 'py_' in x]
     cmd_dict[v] = cmd_list
 # ----------------------------------------------------------------------------------------------------------------------
+print(cmd_dict)
+print(cmd_list)
 
 
 @client.event
 async def on_message(message):
 
     if message.channel.id in allowed_channels:
-        cmd = '_'.join(message.content.split())
+        cmd_args = message.content.split()
+        args = [x for x in cmd_args if '$' in x]
+        cmd = '_'.join([y for y in cmd_args if '$' not in y])
         for key in cmd_dict:
             if cmd in cmd_dict[key]:
-                print(key)
-                print(cmd)
                 exec('a = ' + key + '(client, message)')
-                await eval('a.' + cmd + '()')
+                try:
+                    await eval('a.' + cmd + '(*args)')
+                except TypeError:
+                    msg = 'This function requires ' + str(eval('len(inspect.signature(a.' + cmd + ').parameters)')) +\
+                          ' parameters.\nYou provided ' + str(len(args)) + '.'
+                    await client.send_message(message.channel, msg)
 
 
 @client.event
---
+++ mods/CookieMod.py
@@ -0,0 +1,26 @@
+import realcookie
+
+
+class CookieMod:
+    def __init__(self, client, message):
+        self.client = client
+        self.message = message
+        self.cookie = realcookie.cookie()
+
+    async def py_cookie(self):
+        await self.client.send_file(self.message.channel, fp=self.cookie.gib())
+
+    async def py_cookie_add(self, img_url):
+        self.cookie_add(img_url)
+
+    async def py_cookie_count(self):
+        cookie_num = self.cookie.count()
+        if cookie_num == 1:
+            msg = 'There is `1` cookie in the database.'
+        else:
+            msg = 'There are `' + str(cookie_num) + '` cookies in the database'
+        await self.client.send_message(self.message.channel, msg)
+
+    async def py_cookie_rm(self):
+        self.cookie.rm()
+
