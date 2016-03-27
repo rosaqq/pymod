2cd4482667223b42d7b8acf38e4d125da2f6d75a
Jeremy.Hutchinson <cower3.1@gmail.com>
2016-03-27 00:26:00 -0400

    fixed py_g alias

--- mods/GoogleMod.py
+++ mods/GoogleMod.py
@@ -44,4 +44,4 @@ class GoogleMod:
             await self.client.send_message(self.message.channel, "Couldn't find anything ;-;")
 
     async def py_g(self, *args):
-        self.py_search(args)
+        await self.py_search(args)
