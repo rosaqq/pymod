9735629af7f0924e60081db7e0bb00deb01eef3a
Jeremy.Hutchinson <cower3.1@gmail.com>
2016-04-08 15:42:18 -0400

    chenged Exception handling

--- core.py
+++ core.py
@@ -162,9 +162,9 @@ async def on_message(message):
                 await safe_send(message.author, msg)
                 await client.send_message(message.channel, "<@" + message.author.id + "> Check your DMs for help")
 
-        except Exception as retard:
-            msg = 'Something went wrong:\n' + str(retard)
-            print("{}{:>20}{:<10}{}{:<15}{}{}".format(time.strftime("%Y-%m-%d %H:%M:%S"), " EXCEPTION: ", message.author.name[:10], "| Channel: ", message.channel.name[:15], "| Msg: ", message.content))
+        except Exception as e:
+            msg = str(e.__type__) + ": " + str(e)
+            print("{}{:>20}{:<10}{}{:<15}{}{}{}{}".format(time.strftime("%Y-%m-%d %H:%M:%S"), " EXCEPTION: ", message.author.name[:10], "| Channel: ", message.channel.name[:15], "| Msg: ", message.content, "| ErrorMsg: ", msg))
             await client.send_message(message.channel, msg)
             # print("%s%s" % (fg(6), bg(1)) + time.strftime("%Y-%m-%d %H:%M:%S") + " EXCEPTION: " + message.author.name + "| command execpted: " + cmd + " with args: " + " ".join(args) + "%s" % (attr(0)))
 
